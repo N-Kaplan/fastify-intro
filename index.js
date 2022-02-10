217dc69aa170a6c7184e1bc65c054ac8d4d44462
Naama Kaplan <nkaplany@gmail.com>
2022-02-10 13:41:36 +0100

    generics, defining interfaces for routes

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 "use strict";
+//reference: https://www.fastify.io/docs/latest/Reference/TypeScript/
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const fastify_1 = __importDefault(require("fastify"));
+//1. setup server
 const server = (0, fastify_1.default)();
 server.get('/ping', async (request, reply) => {
     return 'pong\n';
@@ -14,4 +16,17 @@ server.listen(8080, (err, address) => {
         process.exit(1);
     }
     console.log(`Server listening at ${address}`);
+});
+//define new API route
+server.get('/auth', {
+    //add preValidation hook
+    preValidation: (request, reply, done) => {
+        const { username, password } = request.query;
+        done(username !== 'admin' ? new Error('Must be admin') : undefined); // only validate admin account
+    }
+}, async (request, reply) => {
+    const { username, password } = request.query;
+    const customerHeader = request.headers['h-Custom'];
+    // do something with request data
+    return `logged in!`;
 });
--- index.ts
+++ index.ts
@@ -1,5 +1,9 @@
-import fastify from "fastify";
+//reference: https://www.fastify.io/docs/latest/Reference/TypeScript/
 
+import fastify from "fastify";
+import {request} from "https";
+
+//1. setup server
 const server = fastify();
 
 server.get('/ping', async (request, reply) => {
@@ -12,4 +16,35 @@ server.listen(8080, (err, address) => {
         process.exit(1)
     }
     console.log(`Server listening at ${address}`)
+})
+
+//2. Generics
+//define interfaces
+
+interface IQuerystring {
+    username: string;
+    password: string;
+}
+
+interface IHeaders {
+    'h-Custom': string;
+}
+
+//define new API route
+server.get<{
+    Querystring: IQuerystring,
+    Headers: IHeaders
+}>('/auth', {
+    //add preValidation hook
+    preValidation: (request, reply, done) => {
+        const { username, password } = request.query
+        done(username!=='admin' ? new Error('Must be admin') : undefined) // only validate admin account
+    }
+
+}, async (request, reply) => {
+    const { username, password } = request.query
+    const customerHeader = request.headers['h-Custom']
+    // do something with request data
+
+    return `logged in!`
 })
